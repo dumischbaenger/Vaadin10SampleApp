084b030be244d172a25175f68bd6c5ec6ee00c40
bdonauba <bdonauba@gmail.com>
2018-09-18 16:51:55 +0200

    Tutorial Step1

--- src/main/groovy/de/dumischbaenger/MainView.groovy
+++ src/main/groovy/de/dumischbaenger/MainView.groovy
@@ -1,6 +1,7 @@
 package de.dumischbaenger;
 
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.dependency.HtmlImport
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
@@ -8,11 +9,17 @@ import com.vaadin.flow.router.Route;
 /**
  * The main view contains a button and a click listener.
  */
-@Route
+@HtmlImport("styles/shared-styles.html")  
+@Route("")
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me now", {event -> Notification.show("Clicked me now!")});
-        add(button);
+        Button button = new Button("Click me now", {
+          event -> 
+          Notification.show("Clicked me now!")
+        })
+        add(button)
+        
+        setClassName("main-layout")
     }
 }
